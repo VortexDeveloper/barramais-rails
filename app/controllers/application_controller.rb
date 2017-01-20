443f3a0d93013a90f91bcb822dd49424899649f7
= <=>
2017-01-20 19:23:54 -0200

    devise responding json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :verified_request?
+
+  def verified_request?
+    if request.content_type == "application/json"
+      true
+    else
+      super
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :user
+  belongs_to :user, optional: true
 end
