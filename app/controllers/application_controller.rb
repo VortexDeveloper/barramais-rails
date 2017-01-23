f7322d4f2c507bb9fe3611b2afe578e3883e54cf
= <=>
2017-01-23 17:13:47 -0200

    half tested user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :verified_request?
 
+  private
+  
   def verified_request?
     if request.content_type == "application/json"
       true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :user, optional: true
+  belongs_to :partner, class_name: User, optional: true
+  enum relationship: [:single, :serious_relationship, :married]
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates_date :birthday, on_or_before: 18.years.ago, allow_nil: true, on: :create
 end
