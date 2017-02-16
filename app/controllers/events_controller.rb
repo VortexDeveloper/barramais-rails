5fce1a1ffb6e83d4a215deeb221e8fe6b52f889e
Arthur Stefe <arthur.stefe@gmail.com>
2017-02-16 14:23:46 -0200

    Merge Eventes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,20 +2,10 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy, :save_cover_photo]
 
 
-  def save_cover_photo
+  def save_cover_photo event
     image = Paperclip.io_adapters.for(cover_photo_params[:image])
     image.original_filename = "#{cover_photo_params[:filename]}"
-    @event.cover_photo = image
-
-    respond_to do |format|
-      if @event.save
-        format.html {}
-        format.json { render json: @event }
-      else
-        format.html {}
-        format.json { render json: {errors: {cover_photo: ['não foi possível salvar']}}.to_json }
-      end
-    end
+    event.cover_photo = image
   end
 
   def country_for_select
@@ -70,6 +60,7 @@ class EventsController < ApplicationController
     @event.create_address(address_params)
     respond_to do |format|
       if @event.save
+        save_cover_photo(@event)
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
         format.json { render json: @event.to_json }
       else
@@ -120,7 +111,10 @@ class EventsController < ApplicationController
     end
 
     def cover_photo_params
-      params.require(:cover_photo).permit(:image, :filename)
+      params.require(:cover_photo).permit(
+        :image,
+        :filename
+      )
     end
 
     def address_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   put 'users/:id/save_avatar' => 'users#save_avatar', as: 'user_save_avatar'
   put 'groups/:id/save_cover_photo' => 'groups#save_cover_photo', as: 'groups_save_cover_photo'
-  put 'events/:id/save_cover_photo' => 'events#save_cover_photo', as: 'events_save_cover_photo'
+  #put 'events/:id/save_cover_photo' => 'events#save_cover_photo', as: 'events_save_cover_photo'
   get "users/index"
   resources :groups
 
