88068e9e7d0fb59bb119ad522992d35a7e843e8a
Arthur Stefe <arthur.stefe@gmail.com>
2017-02-17 20:49:38 -0200

    Convidados

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   put 'groups/:id/save_cover_photo' => 'groups#save_cover_photo', as: 'groups_save_cover_photo'
   #put 'events/:id/save_cover_photo' => 'events#save_cover_photo', as: 'events_save_cover_photo'
   get "users/index"
+  get 'users/friends'
   resources :groups
 
   resources :events do
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
       get 'country_for_select' => 'events#country_for_select'
       get 'cities_for_select/:id' => 'events#cities_for_select'
       get 'states_for_select/:id' => 'events#states_for_select'
+      put 'invitation/:id' => 'events#invitation'
     end
   end
 
