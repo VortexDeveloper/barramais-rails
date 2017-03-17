1d8e8b17814bacdf61dac4bca9766466c0f99a21
= <=>
2017-03-16 22:15:23 -0300

    removing js in .gitignore

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,3 @@ Rails.application.config.assets.version = '1.1'
 # application.js, application.css, and all non-JS/CSS in app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile += %w( application.js )
