d47cd6247692ac2873228df6de467c194d0d099c
Thiago Ururay <taraujo@id.uff.br>
2017-03-21 05:29:55 -0300

    User friendship API

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,9 @@
 class Friendship < ApplicationRecord
-
-  before_save :initialize_status
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+
+  before_create :initialize_status
+  after_save :reflect_friendship
 
   enum status: [
     :pending,
@@ -10,8 +11,22 @@ class Friendship < ApplicationRecord
     :refuse
   ]
 
+  validates :friend, uniqueness: { scope: :user }
+
+  private
+
   def initialize_status
-    self.status = :pending if self.status.nil?
+    self.status ||= :pending
   end
 
+  def reflect_friendship
+    if accept? && !friend.friend_of?(user)
+      friend.friends << user
+      find_reflection.accept!
+    end
+  end
+
+  def find_reflection
+    Friendship.where(user: friend, friend: user).first
+  end
 end
