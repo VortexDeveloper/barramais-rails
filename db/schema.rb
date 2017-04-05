5e3ddeefc3915ed6a03ab52db110501867df5348
PedroDeAlbuquerque <pedro.de.albuquerque@hotmail.com>
2017-04-05 14:36:42 -0300

    trips changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405133910) do
+ActiveRecord::Schema.define(version: 20170405165549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -459,6 +459,8 @@ ActiveRecord::Schema.define(version: 20170405133910) do
     t.string   "cover_photo_content_type"
     t.integer  "cover_photo_file_size"
     t.datetime "cover_photo_updated_at"
+    t.text     "national_trips"
+    t.text     "international_trips"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["partner_id"], name: "index_users_on_partner_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
