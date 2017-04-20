71f203fa3dad132d4840c05145bdac76ba2c15c0
PedroDeAlbuquerque <pedro.de.albuquerque@hotmail.com>
2017-04-20 18:13:43 -0300

    ad dependent, destroy

--- app/models/interest_area.rb
+++ app/models/interest_area.rb
@@ -2,6 +2,6 @@ class InterestArea < ApplicationRecord
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
-  has_many :ad_interests
+  has_many :ad_interests, dependent: :destroy
   has_many :ads, through: :ad_interests
 end
