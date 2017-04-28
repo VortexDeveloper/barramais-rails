2c7e2cfd7f2977fb319e0fcd6938d0bac8515dff
= <=>
2017-04-28 16:51:13 -0300

    photo url to vessel type

--- app/models/vessel_type.rb
+++ app/models/vessel_type.rb
@@ -1,4 +1,10 @@
 class VesselType < ApplicationRecord
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
+  def as_json(options = { })
+    h = super(options)
+    h[:photo_url]   = ApplicationController.helpers.asset_url(photo.url)
+    h
+  end
 end
